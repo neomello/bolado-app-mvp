b6a263dce9ae518865d6c28f52bad2d16031e86b
FlowOFF <nettoaeb1@gmail.com>
2025-06-12 16:05:59 -0300

    🔥 Clean MVP: estrutura mínima e funcional para deploy

--- utils/throwApiError.js
+++
@@ -1,7 +0,0 @@
-export const throwApiError = (mensagem, codigo = 'ERRO_GENERICO', detalhe = null, status = 500) => {
-  const erro = new Error(mensagem);
-  erro.codigo = codigo;
-  erro.detalhe = detalhe;
-  erro.status = status;
-  throw erro;
-}; 
